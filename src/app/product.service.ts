8382a43cb8d6d0ca0d24c873e52bc85fae61022e
meganmuirhead <megan@s56.net>
2018-07-27 19:04:30 -0600

    working on some bugs

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 
 
@@ -14,8 +15,7 @@ export class ProductService {
   }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl);
-    // return Observable.create(this._albumUrl);
+    return this._http.get(this._albumUrl).map((response) => response.json())
   }
 }
 
